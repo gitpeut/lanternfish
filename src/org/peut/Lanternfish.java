c2e8e67694ef49b906386400f442aef99bfb2bd7
gitpeut <peut@peut.org>
2022-02-04 01:01:35 +0100

    Large schools require a numeric approach - first try

--- src/org/peut/Lanternfish.java
+++ src/org/peut/Lanternfish.java
@@ -6,39 +6,42 @@ import java.util.ArrayList;
 public class Lanternfish
 {
     static long[]  school = new long[9];
-    static long   totalFish;
+    static long    schoolSize = 0;
 
     public Lanternfish(  int [] initDays ) {
         for ( int i : initDays){
             school[ initDays[i] ]++;
+            schoolSize++;
         }
     }
 
     void ageSchool(){
-         for ( int i = 1 ; i < school.length - 2 ; --i ){
+         long spawnNumber = 0;
 
-
+         for ( int i = 0 ; i < school.length -1 ; ++i ){
+             if ( i == 0 ){
+                 spawnNumber = school[i];
+                 school[i]   = 0;
+             }
+             school[i] = school[ i + 1 ];
+             if ( i == 6 ){
+                 school[i] += spawnNumber;
+             }
          }
-
+         school[8]   = spawnNumber;
+         schoolSize += spawnNumber;
     }
 
     void showSchool(){
-        System.out.printf(" - total : %4d - ", school.size() );
+        System.out.printf(" total : %16d - ", schoolSize );
 
-        if ( school.size() < 80 ) {
-            for (int i = 0; i < school.size(); ++i) {
-                if (i != 0) System.out.print(",");
-                System.out.print(school.get(i).getDaysUntilSpawn());
-            }
+
+        for (int i = 0; i < school.length; ++i) {
+            System.out.printf( " %16d ", school[i]);
         }
-        System.out.printf("%n");
+
+        System.out.println();
     }
 
-    public int getDaysUntilSpawn() {
-        return daysUntilSpawn;
-    }
 
-    public void setDaysUntilSpawn(int daysUntilSpawn) {
-        this.daysUntilSpawn = daysUntilSpawn;
-    }
 }
